c451853d41e0042b10dea74c0ed9d7eb44dc1147
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2022-01-31 11:06:34 +0200

    news articles

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,17 @@ import Image from "../components/Image";
 import fieldImg from "../assets/img-field.jpg";
 import leavesImg from "../assets/leaves.png";
 import closeUpLips from "../assets/closeup-lips.png";
-import coloredFlag from "../assets/flag-colored.png";
-import whiteFlag from "../assets/flag-white.png";
+import articleOne from "../assets/news-article-one.png"
+import articleTwo from "../assets/news-article-two.png"
+import articleThree from "../assets/news-article-three.png"
 import styles from "../styles/Home.module.css";
+
+const articles = [
+  { id: 1, title: 'Lorem Ipsum Somthing', img: articleOne},
+  { id: 2, title: 'Lorem Ipsum something', img: articleTwo},
+  { id: 3, title: 'Lorem Ipsum something', img: articleThree},
+  { id: 4, title: 'Lorem Ipsum something', img: articleTwo},
+]
 
 export default function Home() {
   return (
@@ -21,10 +29,26 @@ export default function Home() {
           <Image source={fieldImg} styles={styles.field} />
           <Image source={closeUpLips} styles={styles.closeUp} />
           <div className={styles.flags}>
-            <Image source={whiteFlag} />
             <h2>Scroll in on our Retailers Country wide</h2>
           </div>
           <Image source={leavesImg} styles={styles.leaves} />
+        </div>
+
+        <div className={styles.newsContainer}>
+          <hr className={styles.divider} />
+          <h1>News</h1>
+          <div className={styles.articles}>
+            {articles.map(article => {
+              return (
+                <div className={styles.article} key={article.id}>
+                  <Image source={article.img} styles={styles.articleImg} />
+                  <div className={styles.overlay}>
+                    <div className={styles.articleTitle}>{article.title}</div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </main>
     </div>
